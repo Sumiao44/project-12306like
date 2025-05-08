7f856cca4bc5480f0cb78397a5c1190d3263e6f1
Maosuuu <1970793761@qq.com>
2025-05-08 16:01:39 +0800

    支付模块创建

---
+++ services/pay-service/src/main/java/org/project12306/services/payservice/controller/PayCallbackController.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.project12306.services.payservice.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.date.DateUtil;
+import lombok.RequiredArgsConstructor;
+import org.project12306.commons.desingnpattern.strategy.AbstractStrategyChoose;
+import org.project12306.services.payservice.common.enums.PayChannelEnum;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * 支付结果回调
+ */
+@RestController
+@RequiredArgsConstructor
+public class PayCallbackController {
+
+    private final AbstractStrategyChoose abstractStrategyChoose;
+
+    /**
+     * 支付宝回调
+     * 调用支付宝支付后，支付宝会调用此接口发送支付结果
+     */
+    @PostMapping("/api/pay-service/callback/alipay")
+    public void callbackAlipay(@RequestParam Map<String, Object> requestParam) {
+
+    }
+}
